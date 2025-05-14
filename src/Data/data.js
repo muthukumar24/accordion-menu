ff4721e874c63a80b1ee4c4ff4c4b1025eb5e0cb
Muthu kumar <muthukumar24081999@gmail.com>
2025-05-14 23:36:01 +0530

    Development in Progress

---
+++ src/Data/data.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    title: "JavaScript Basics",
+    content: "Learn Datatypes, variables, functions and loops in javascript",
+  },
+  {
+    title: "React.js Basics",
+    content: "Learn State Management, Context-API",
+  },
+  {
+    title: "Node.js Basics",
+    content: "Learn to handle CORS Error",
+  },
+  {
+    title: "Express.js Basics",
+    content: "Learn to create HTTP Server using Express.js",
+  },
+];
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { items } from '../Data/data'
+
+const Accordion = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Accordion
